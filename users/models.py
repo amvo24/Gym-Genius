7913802ee482ea1850ae80927121c88920072c9c
Andrew <andrewvo1024@gmail.com>
2023-02-26 15:21:59 -0600

    User model was created

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
+    email = models.EmailField()
+    weight = models.IntegerField()
+    DOB = models.DateTimeField()
+    age = models.IntegerField()
