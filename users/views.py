122c502dbbd4efe8cfb83f5868de4b8a2b27b13b
Andrew <andrewvo1024@gmail.com>
2023-02-26 17:58:31 -0600

    all crud operations for user is done

--- users/views.py
+++ users/views.py
@@ -5,6 +5,9 @@ import json
 
 
 # Create your views here.
+
+# Creates a user
+@csrf_exempt
 def create_user(request):
     if request.method == 'POST':
         data = json.loads(request.body)
@@ -16,6 +19,70 @@ def create_user(request):
         dob = data.get('DOB')
         age = data.get('age')
         user = User.objects.create(first_name=first_name, last_name=last_name, username=username, email=email, weight=weight, dob=dob, age=age)
+        return JsonResponse({'status': 'success'})
+    else:
+        return JsonResponse({'status': 'error'})
+
+# Get all users
+@csrf_exempt
+def get_all_users(request):
+    users = User.objects.all()
+    data = [{'firstName': user.first_name,
+             'lastName': user.last_name,
+             'username': user.username,
+             'email': user.email,
+             'weight': user.weight,
+             'DOB': user.dob,
+             'age': user.age }
+             for user in users]
+    return JsonResponse({'users': data})
+
+# Get a user
+@csrf_exempt
+def get_user(request, user_id):
+    user = User.objects.get(id=user_id)
+    data = {'firstName': user.first_name,
+            'lastName': user.last_name,
+            'username': user.username,
+            'email': user.email,
+            'weight': user.weight,
+            'DOB': user.dob,
+            'age': user.age}
+    return JsonResponse({'user':data})
+
+# Update a user
+@csrf_exempt
+def update_user(request, user_id):
+    if request.method == 'PUT':
+        data = json.loads(request.body)
+        first_name = data.get('firstName')
+        last_name = data.get('lastName')
+        username = data.get('username')
+        email = data.get('email')
+        weight = data.get('weight')
+        dob = data.get('DOB')
+        age = data.get('age')
+
+        user = User.objects.get(id=user_id)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.username = username
+        user.email = email
+        user.weight = weight
+        user.dob = dob
+        user.age = age
+
+        user.save()
+        return JsonResponse({'status': 'success'})
+    else:
+        return JsonResponse({'status': 'error'})
+
+# Delete a user
+@csrf_exempt
+def delete_user(request, user_id):
+    if request.method == 'DELETE':
+        user = User.objects.get(id=user_id)
+        user.delete()
         return JsonResponse({'status': 'success'})
     else:
         return JsonResponse({'status': 'error'})
