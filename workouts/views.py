716ddfa41bac1101d2afe8632d9aea6a9a6b68d2
Andrew <andrewvo1024@gmail.com>
2023-02-27 17:33:28 -0600

    working on views for workouts

--- workouts/views.py
+++ workouts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Workout
+import json
+
 
 # Create your views here.
+# def create_workout(request):
+#     if request.method == 'POST':
+#         name = request.POST.get('name')
+#         description = request.POST.get('description')
+#         sets = request.POST.get('sets')
+#         reps = request.POST.get('reps')
+#         exercises = request.POST.getlist('exercises')
+
+#         workout = Workout.objects.create(
+#             name=name,
+#             description=description,
+#             sets=sets,
+#             reps=reps
+#         )
+
+#         for exercise_id in exercises: 
+#             workout.exercises.add(exercise_id)
+
+#         return HttpResponseRedirect('/workouts/')
