8774fc418cbd367b7a7c2c11510c2d63e7e5f950
Andrew <andrewvo1024@gmail.com>
2023-02-26 18:42:05 -0600

    url patterns created and configured to views

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 #URL conf module
 urlpatterns = [
-
+    path('createuser/', views.create_user),
+    path('getallusers/', views.get_all_users),
+    path('getuser/', views.get_user),
+    path('updateuser/', views.update_user),
+    path('deleteuser/', views.delete_user)
 ]
