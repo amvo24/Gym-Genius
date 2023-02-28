a124d7aad27649c06a5d3386ea1f27ad4287ecc0
Andrew Vo <andrewvo@Andrews-MacBook-Pro-2.local>
2023-02-27 21:36:39 -0600

    create op for workout done

--- workouts/views.py
+++ workouts/views.py
@@ -5,22 +5,19 @@ import json
 
 
 # Create your views here.
-# def create_workout(request):
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         description = request.POST.get('description')
-#         sets = request.POST.get('sets')
-#         reps = request.POST.get('reps')
-#         exercises = request.POST.getlist('exercises')
 
-#         workout = Workout.objects.create(
-#             name=name,
-#             description=description,
-#             sets=sets,
-#             reps=reps
-#         )
-
-#         for exercise_id in exercises: 
-#             workout.exercises.add(exercise_id)
-
-#         return HttpResponseRedirect('/workouts/')
+# Creates a workout
+@csrf_exempt
+def create_workout(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        name = data.get('name')
+        description = data.get('description')
+        sets = data.get('sets')
+        reps = data.get('reps')
+        exercises = data.get('exercises')
+        workout = Workout.objects.create(name=name, description=description, sets=sets, reps=reps)
+        workout.exercises.set(exercises)
+        return JsonResponse({'status': 'success'})
+    else:
+        return JsonResponse({'status': 'error'})
