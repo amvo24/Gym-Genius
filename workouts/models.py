fb5e2836d772d6bbc434e92a976ce6acae13cfe2
Andrew <andrewvo1024@gmail.com>
2023-02-28 00:04:26 -0600

    added a foreign key to the workout model to reference users table

--- workouts/models.py
+++ workouts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users.models import User
 from exercises.models import Exercise, Muscle
 
 # Create your models here.
@@ -9,6 +10,7 @@ class Workout(models.Model):
     reps = models.IntegerField()
     exercises = models.ManyToManyField(Exercise, related_name='workouts')
     muscles_worked = models.ManyToManyField(Muscle, related_name='workouts')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
