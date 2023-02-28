5b0936d2d437323f3df124e0540927ead8b104c6
Andrew <andrewvo1024@gmail.com>
2023-02-27 23:52:43 -0600

    views completed for workouts app

--- workouts/views.py
+++ workouts/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from .models import Workout
+from .models import Workout, Exercise
 import json
 
 
@@ -21,3 +21,53 @@ def create_workout(request):
         return JsonResponse({'status': 'success'})
     else:
         return JsonResponse({'status': 'error'})
+
+# Gets a workout
+@csrf_exempt
+def get_workout(request, workout_id):
+    try:
+        workout = Workout.objects.get(id=workout_id)
+        data = {
+            'id': workout.id,
+            'name': workout.name,
+            'description': workout.description,
+            'sets': workout.sets,
+            'reps': workout.reps,
+            'exercises': list(workout.exercises.values()),
+        }
+        return JsonResponse({'status': 'success', 'data': data})
+    except Workout.DoesNotExist:
+        return JsonResponse({'status': 'error', 'message': 'Workout does not exist'})
+
+
+# Updates a workout
+@csrf_exempt
+def update_workout(request, workout_id):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        try:
+            workout = Workout.objects.get(id=workout_id)
+            workout.name = data.get('name', workout.name)
+            workout.description = data.get('description', workout.description)
+            workout.sets = data.get('sets', workout.sets)
+            workout.reps = data.get('reps', workout.reps)
+            if 'exercises' in data:
+                exercises = Exercise.objects.filter(id__in=data['exercises'])
+                workout.exercises.set(exercises)
+            workout.save()
+            return JsonResponse({'status': 'success'})
+        except Workout.DoesNotExist:
+            return JsonResponse({'status': 'error', 'message': 'Workout does not exist'})
+    else:
+        return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
+
+
+# Deletes a workout
+@csrf_exempt
+def delete_workout(request, workout_id):
+    try:
+        workout = Workout.objects.get(id=workout_id)
+        workout.delete()
+        return JsonResponse({'status': 'success'})
+    except Workout.DoesNotExist:
+        return JsonResponse({'status': 'error', 'message': 'Workout does not exist'})
