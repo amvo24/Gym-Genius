fb9cfda3e04a239978df597e1dbc78bd445666c3
Andrew Vo <andrewvo@Andrews-MacBook-Pro-2.local>
2023-03-01 00:39:17 -0600

    syntax error fixed

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
 from django.contrib.auth.decorators import login_required
