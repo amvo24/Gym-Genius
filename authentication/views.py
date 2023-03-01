d0bd2890492614a94e330d9fd81152b1a9db54a1
Andrew Vo <andrewvo@Andrews-MacBook-Pro-2.local>
2023-03-01 00:38:24 -0600

    auth views for token, signup, login, logout created

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
+from django.contrib.auth.decorators import login_required
+from django.middleware.csrf import get_token
+
 
 # Create your views here.
+
+@ensure_csrf_cookie
+def get_csrf(request):
+    response = JsonResponse({'detail': 'CSRF cookie set'})
+    return response
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+        user = User.objects.create_user(username, email, password)
+        user.save()
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return JsonResponse({'user_id': user.id})
+    return JsonResponse({'error': 'Invalid request method'})
+
+
+
+@csrf_exempt
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return JsonResponse({'message': 'Logged in successfully'})
+        else:
+            return JsonResponse({'error': 'Invalid credentials'}, status=401)
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return JsonResponse({'success': 'User logged out'})
