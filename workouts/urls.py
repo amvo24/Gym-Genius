1018d8a5c101b1cd4c074ce6c77ca2eda67deac7
Andrew Vo <andrewvo@Andrews-MacBook-Pro-2.local>
2023-03-01 16:31:44 -0600

    url paths created for workout views

---
+++ workouts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('create/', views.create_workout),
+    path('<int:workout_id>/', views.get_workout),
+    path('<int:workout_id>/update/', views.update_workout),
+    path('<int:workout_id>/delete', views.delete_workout),
+]
