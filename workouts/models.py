806eaa46b65faf7d50b2c186481ec96b2d4db226
Andrew Vo <andrewvo@Andrews-MacBook-Pro-2.local>
2023-10-03 17:07:57 -0500

    coming back after a long break

--- workouts/models.py
+++ workouts/models.py
@@ -1,15 +1,81 @@
+# from django.db import models
+# from profiles.models import UserProfile
+# from exercises.models import Exercise, Muscle
+
+# # Create your models here.
+# class Workout(models.Model):
+#     name = models.CharField(max_length=50)
+#     description = models.TextField()
+#     sets = models.IntegerField()
+#     reps = models.IntegerField()
+#     exercises = models.ManyToManyField(Exercise, related_name='workouts')
+#     muscles_worked = models.ManyToManyField(Muscle, related_name='workouts')
+#     profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.name
+
+
+# class Workout(models.Model):
+#     name = models.CharField(max_length=50)
+#     description = models.TextField()
+#     profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.name
+
+
+# class ExerciseInstance(models.Model):
+#     exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+#     sets = models.IntegerField()
+#     reps = models.IntegerField()
+#     description = models.TextField(blank=True)
+#     workout = models.ForeignKey(Workout, on_delete=models.CASCADE, related_name='exercise_instances')
+#     muscles_worked = models.ManyToManyField(Muscle, related_name='exercise_instances')
+
+#     def __str__(self):
+#         return f"{self.exercise.name} ({self.sets}x{self.reps})"
+
 from django.db import models
 from profiles.models import UserProfile
 from exercises.models import Exercise, Muscle
 
-# Create your models here.
-class Workout(models.Model):
+class WorkoutDay(models.Model):
+    DAY_CHOICES = [
+        ('Monday', 'Monday'),
+        ('Tuesday', 'Tuesday'),
+        ('Wednesday', 'Wednesday'),
+        ('Thursday', 'Thursday'),
+        ('Friday', 'Friday'),
+        ('Saturday', 'Saturday'),
+        ('Sunday', 'Sunday'),
+    ]
     name = models.CharField(max_length=50)
     description = models.TextField()
+    day = models.CharField(max_length=10, choices=DAY_CHOICES)
+    position = models.IntegerField()
+    exercises = models.ManyToManyField(Exercise, through='ExerciseInstance')
+    muscles_worked = models.ManyToManyField(Muscle, related_name='workout_days')
+
+    def __str__(self):
+        return f'{self.name} ({self.day})'
+
+class ExerciseInstance(models.Model):
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    workout_day = models.ForeignKey(WorkoutDay, on_delete=models.CASCADE)
     sets = models.IntegerField()
     reps = models.IntegerField()
-    exercises = models.ManyToManyField(Exercise, related_name='workouts')
-    muscles_worked = models.ManyToManyField(Muscle, related_name='workouts')
+    description = models.TextField(blank=True)
+    muscles_worked = models.ManyToManyField(Muscle, related_name='exercise_instances')
+
+    def __str__(self):
+        return f'{self.exercise} ({self.workout_day})'
+
+class WorkoutPlan(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    days_per_week = models.IntegerField()
+    workout_days = models.ManyToManyField(WorkoutDay)
     profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 
     def __str__(self):
--- workouts/views.py
+++ workouts/views.py
@@ -1,32 +1,64 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from .models import Workout, Exercise
+from .models import WorkoutDay, Exercise, ExerciseInstance
 import json
 
 
 # Create your views here.
 
 # Creates a workout compliled of several exercises
+# @csrf_exempt
+# def create_workout(request):
+#     if request.method == 'POST':
+#         data = json.loads(request.body)
+#         name = data.get('name')
+#         description = data.get('description')
+#         sets = data.get('sets')
+#         reps = data.get('reps')
+#         exercises = data.get('exercises')
+#         workout = Workout.objects.create( name=name, description=description, sets=sets, reps=reps)
+#         workout.exercises.set(exercises)
+#         return JsonResponse({'status': 'success'})
+#     else:
+#         return JsonResponse({'status': 'error'})
+
 @csrf_exempt
 def create_workout(request):
     if request.method == 'POST':
         data = json.loads(request.body)
         name = data.get('name')
         description = data.get('description')
-        sets = data.get('sets')
-        reps = data.get('reps')
-        exercises = data.get('exercises')
-        workout = Workout.objects.create( name=name, description=description, sets=sets, reps=reps)
-        workout.exercises.set(exercises)
+        exercise_instances = data.get('exercise_instances')
+        profile_id = data.get('profile_id')
+
+        workout = WorkoutDay.objects.create(name=name, description=description, profile_id=profile_id)
+
+        for ex_inst_data in exercise_instances:
+            exercise_id = ex_inst_data.get('exercise_id')
+            sets = ex_inst_data.get('sets')
+            reps = ex_inst_data.get('reps')
+            description = ex_inst_data.get('description', '')
+            muscles_worked = ex_inst_data.get('muscles_worked', [])
+
+            exercise_instance = ExerciseInstance.objects.create(
+                exercise_id=exercise_id,
+                sets=sets,
+                reps=reps,
+                description=description,
+                workout=workout
+            )
+            exercise_instance.muscles_worked.set(muscles_worked)
+
         return JsonResponse({'status': 'success'})
     else:
         return JsonResponse({'status': 'error'})
+
 
 # Gets a workout
 @csrf_exempt
 def get_workout(request, workout_id):
     try:
-        workout = Workout.objects.get(id=workout_id)
+        workout = WorkoutDay.objects.get(id=workout_id)
         data = {
             'id': workout.id,
             'name': workout.name,
@@ -36,7 +68,7 @@ def get_workout(request, workout_id):
             'exercises': list(workout.exercises.values()),
         }
         return JsonResponse({'status': 'success', 'data': data})
-    except Workout.DoesNotExist:
+    except WorkoutDay.DoesNotExist:
         return JsonResponse({'status': 'error', 'message': 'Workout does not exist'})
 
 
@@ -46,7 +78,7 @@ def update_workout(request, workout_id):
     if request.method == 'POST':
         data = json.loads(request.body)
         try:
-            workout = Workout.objects.get(id=workout_id)
+            workout = WorkoutDay.objects.get(id=workout_id)
             workout.name = data.get('name', workout.name)
             workout.description = data.get('description', workout.description)
             workout.sets = data.get('sets', workout.sets)
@@ -56,7 +88,7 @@ def update_workout(request, workout_id):
                 workout.exercises.set(exercises)
             workout.save()
             return JsonResponse({'status': 'success'})
-        except Workout.DoesNotExist:
+        except WorkoutDay.DoesNotExist:
             return JsonResponse({'status': 'error', 'message': 'Workout does not exist'})
     else:
         return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
@@ -66,8 +98,8 @@ def update_workout(request, workout_id):
 @csrf_exempt
 def delete_workout(request, workout_id):
     try:
-        workout = Workout.objects.get(id=workout_id)
+        workout = WorkoutDay.objects.get(id=workout_id)
         workout.delete()
         return JsonResponse({'status': 'success'})
-    except Workout.DoesNotExist:
+    except WorkoutDay.DoesNotExist:
         return JsonResponse({'status': 'error', 'message': 'Workout does not exist'})
